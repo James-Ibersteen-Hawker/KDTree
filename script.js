47eeae46f3b9fbcf516a5c8053a37312eda850b3
James Hawker <526rserbinenko@frhsd.com>
2026-05-11 18:21:29 +0000

    private fields, finished binary conversion

--- script.js
+++ script.js
@@ -4,31 +4,36 @@ const data2 = [
     [3,4,5]
 ]
 class Ntree extends Array {
+    #cutoff;
+    #data;
     constructor(data) {
         if (!data.every(e => e.every(Number.isInteger))) throw new Error("Invalid input!")
         super(...data);
-        this._data = this.convert(data);
+        this.#cutoff = 65535;
+        this.#data = this.#convert(data);
     }
     set set(data) {
         this.length = 0;
         this.push(...data);
-        this._data = this.convert(data);
+        this.#data = this.#convert(data);
     }
-    convert(data) {
+    #convert(data) {
         const payload = [...new Map(data.map(e => [JSON.stringify(e), e])).values()];
         const l = payload[0].length;
         if (!payload.every(e => e.length === l)) throw new Error("Inequal lengths");
         const basis = payload.flat();
-        const modded = new Uint8Array(basis * 2);
+        const modded = new Uint8Array(basis.length * 2);
         for (let i = 0, j = 0; i < basis.length; i++, j+= 2) {
-            const current = basis[i];
-            //move 1 in basis, and move 2 in modded
+            if (basis[i] > this.#cutoff) throw new Error(`${basis[i]} is greater than ${this.#cutoff}`)
+            const [low, high] = [basis[i] & 255, basis[i] >> 8]
+            modded[j] = low;
+            modded[j + 1] = high;
         }
-        return payload;
+        return modded;
     }
     static get [Symbol.species]() {
         return Array;
     }
 }
+
 const test = new Ntree(data);
-alert(test)
