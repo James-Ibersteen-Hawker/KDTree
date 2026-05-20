8a1c3b1828b89765cd703ffa8ce0724947d039e9
James Hawker <526rserbinenko@frhsd.com>
2026-05-20 13:42:56 +0000

    2.0 begins

--- tree2.js
+++ tree2.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 Plan:
 - Use Float32Arrays
@@ -6,3 +7,28 @@ Plan:
 - Keep the array-of-indexes approach and the flatpacked storage
 - Child bounds are in parallel arrays, similarly processed to the flatpacked storage
 */
+import xxhash from "https://unpkg.com/xxhash-wasm/esm/xxhash-wasm.js";
+const hash = xxhash();
+async function validate(data) {
+    const { h32 } = await hash;
+    const map = new Map();
+    for (let i = 0; i < data.length; i++) {
+        alert(data[i])
+        
+    }
+}
+export default class KDTree2 {
+    #data;
+    #indexes;
+    #length;
+    constructor(data) {
+        this.ready = this.#init(data);
+    }
+    async #init(data) {
+        if (!data[0]) throw new Error("First element doesn't exist");
+        this.#length = data[0]?.length;
+        if (this.#length == null) throw new Error("Invalid starting length");
+        this.#data = await validate(data);
+        //now it is all the proper length
+    }
+}
