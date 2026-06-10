15e4fd71bd1834c1f6dbe1d3c715cc11b611bca1
James Hawker <181120354+James-Ibersteen-Hawker@users.noreply.github.com>
2026-06-10 18:10:53 +0000

    benchmarking continues!

--- src/init.js
+++ src/init.js
@@ -26,24 +26,37 @@ import d1000000p from "./benchmark_sets/1000000p.js";
 //   `const d${l}p = ${JSON.stringify(list)};\nexport default d${l}p;`
 // );
 const q = [300 * Math.E, 300.4323834, Math.PI];
+const iterations = 500;
 const benchmarks = [];
-const lists = [d10p, d100p, d1000p, d10000p, d100000p, d1000000p]
+const lists = [d10p, d100p, d1000p, d10000p, d100000p]
 for (let i = 0; i < lists.length; i++) {
     const testList = lists[i];
     const testOBJ = {
-        construction: null,
-        search: null,
+        "construction (in ms)": null,
+        "search (in ms)": null,
         size: testList.length,
         result: null
     }
-    const buildStart = performance.now();
-    const TestTree = await KDTree.initFrom(testList);
-    const buildEnd = performance.now();
-    testOBJ.construction = buildEnd - buildStart;
-    const searchStart = performance.now();
-    testOBJ.result = TestTree.search(q);
-    const searchEnd = performance.now();
-    testOBJ.search = searchEnd - searchStart;
+    let TestTree;
+    //construction
+    let cumulativeBuildTime = 0;
+    for (let d = 0; d < iterations; d++) {
+        const buildStart = performance.now();
+        TestTree = await KDTree.initFrom(testList);
+        const buildEnd = performance.now();
+        cumulativeBuildTime += buildEnd - buildStart;
+    }
+    testOBJ["construction (in ms)"] = cumulativeBuildTime / iterations;
+    //searching
+    let cumulativeSearchTime = 0;
+    for (let d = 0; d < iterations; d++) {
+        const searchStart = performance.now();
+        testOBJ.result = TestTree.search(q, {});
+        const searchEnd = performance.now();
+        cumulativeSearchTime += searchEnd - searchStart;
+    }
+    testOBJ["search (in ms)"] = cumulativeSearchTime / iterations;
     benchmarks.push(testOBJ);
 }
+console.log(q);
 console.table(benchmarks)
