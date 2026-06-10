bb07f8511b334e74134c17b948eee0892994d498
James Hawker <181120354+James-Ibersteen-Hawker@users.noreply.github.com>
2026-06-10 17:42:18 +0000

    made more datasets, about to benchmark

--- src/init.js
+++ src/init.js
@@ -1,14 +1,49 @@
 import KDTree from "./kdtree.js";
-// import BigSet from "./benchmark.js"
-import _4Dset from "./points_4d.js"
-// import SmallSet from "./data.js"
-//////
-const newTree = await KDTree.initFrom(_4Dset);
-const q = [250,15, 590, 256];
-const result = newTree.search(q, {
-    includeDistance: true
-})
-console.log(q, result);
+// import d50k3d from "./datasets/50k3d.js"
+// import d50k4d from "./datasets/50k4d.js"
+// import d603d from "./datasets/603d.js"
+// import { writeFile } from 'node:fs/promises';
+import d10p from "./benchmark_sets/10p.js";
+import d100p from "./benchmark_sets/100p.js";
+import d1000p from "./benchmark_sets/1000p.js";
+import d10000p from "./benchmark_sets/10000p.js";
+import d100000p from "./benchmark_sets/100000p.js";
+import d1000000p from "./benchmark_sets/1000000p.js";
 
-// const serialTree = newTree.serialize("es6-typed");
-// const deserialTree = await KDTree.initFromSerial(serialTree)
+
+// const list = [];
+// const l = 1000000;
+// for (let i = 0; i < l; i++) {
+//     const point = [];
+//     for (let d = 0; d < 3; d++) {
+//         const random = Math.random() * 1000;
+//         point.push(random);
+//     }
+//     list.push(point);
+// }
+// await writeFile(
+//   `./src/benchmark_sets/${l}p.js`,
+//   `const d${l}p = ${JSON.stringify(list)};\nexport default d${l}p;`
+// );
+const q = [300 * Math.E, 300.4323834, Math.PI];
+const benchmarks = [];
+const lists = [d10p, d100p, d1000p, d10000p, d100000p, d1000000p]
+for (let i = 0; i < lists.length; i++) {
+    const testList = lists[i];
+    const testOBJ = {
+        construction: null,
+        search: null,
+        size: testList.length,
+        result: null
+    }
+    const buildStart = performance.now();
+    const TestTree = await KDTree.initFrom(testList);
+    const buildEnd = performance.now();
+    testOBJ.construction = buildEnd - buildStart;
+    const searchStart = performance.now();
+    testOBJ.result = TestTree.search(q);
+    const searchEnd = performance.now();
+    testOBJ.search = searchEnd - searchStart;
+    benchmarks.push(testOBJ);
+}
+console.table(benchmarks)
